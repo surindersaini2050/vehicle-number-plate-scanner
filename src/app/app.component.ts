0e0c3aecfe8945f61c7a49889205f4fb868985ba
Surinder Saini <surinder.saini2050@gmail.com>
2024-12-29 23:19:01 +0530

    menu bar setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { MatSharedModule } from './common/mat-shared/mat-shared.module';
 import { HeaderComponent } from './common/components/header/header.component';
 import { FooterComponent } from './common/components/footer/footer.component';
@@ -9,14 +9,14 @@ import { FooterComponent } from './common/components/footer/footer.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, MatSharedModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MatSharedModule, HeaderComponent, FooterComponent, RouterModule],
 })
 export class AppComponent {
   isDesktop: boolean = true;
   loading:boolean = true;
   menuOptions: any = [
-    {name: 'Scan', value: 'scan', icon: 'list_alt'},
-    {name: 'Scanned List', value: 'scanned-list', icon: 'camera_alt'},
+    {name: 'Scan', RouterLink: '/scan', icon: 'camera_alt'},
+    {name: 'Scanned List', RouterLink: '/scanned-list', icon: 'list_alt'},
   ]
   
   constructor() { }
